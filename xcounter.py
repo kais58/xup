2d55f42d659a150c75415a11dbc155a7fd32ad4d
Callum Massey <kais58@sucs.org>
2015-01-27 00:21:07 +0000

    Default to opening most recent modified fleet log if no arguments given

--- xcounter.py
+++ xcounter.py
@@ -1,6 +1,8 @@
-import codecs, sys, datetime, logging
+import codecs, sys, datetime, logging, os, glob
 from filetail import FileTail
-if __name__ == "__main__":
+from win32com.shell import shell, shellcon
+
+def xcounter(filename):
     log = logging.getLogger(__name__)
     out_hdlr = logging.StreamHandler(sys.stdout)
     out_hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
@@ -8,11 +10,11 @@ if __name__ == "__main__":
     log.addHandler(out_hdlr)
     log.setLevel(logging.INFO)
 
-    tail = FileTail(sys.argv[1])
+    tail = FileTail(filename)
 
     count = 0
     time = None
-    log.info("Initiated xcounter on %s" % sys.argv[1])
+    log.info("Initiated xcounter on %s" % filename)
     for line in tail:
         line = line.rstrip("\r\n")
         if line.count(" -"):
@@ -22,3 +24,17 @@ if __name__ == "__main__":
         elif line.count(" x"):
             count += 1
             log.info(str(count))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        xcounter(sys.argv[1])
+
+    else:
+        fleetlogs = {}
+        os.chdir(shell.SHGetFolderPath(0, shellcon.CSIDL_PERSONAL, None, 0) + "\EVE\logs\Chatlogs")
+        for file in glob.glob("Fleet*"):
+            fleetlogs[file] = os.path.getmtime(file)
+
+        maxval = max(fleetlogs, key=fleetlogs.get)
+        xcounter(shell.SHGetFolderPath(0, shellcon.CSIDL_PERSONAL, None, 0) + "\EVE\logs\Chatlogs\\" + maxval)
